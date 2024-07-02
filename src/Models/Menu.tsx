ddff2c547436d951d1824a229cff261b18d9f335
SakalikPeter <sakalikp@gmail.com>
2024-07-02 22:05:07 +0200

    calculate menu nutrition

--- src/Models/Menu.tsx
+++ src/Models/Menu.tsx
@@ -1,3 +1,7 @@
+import { Food } from "./Food";
+import { Recipe } from "./Recipe";
+import { RecipeNutritions } from "./RecipeNutritions";
+
 export class SelectedItems {
   key: number;
   quantity: number
@@ -12,11 +16,13 @@ export class Menu {
   date: string;
   foods?: SelectedItems[];
   recipes?: SelectedItems[];
+  nutritions: RecipeNutritions;
 
   constructor(date: string, foods?, recipes?) {
     this.date = date;
     this.foods = foods ?? [];
     this.recipes = recipes ?? [];
+    this.nutritions = new RecipeNutritions();
   }
 
   public getFoodKeys(): number[] {
@@ -25,9 +31,42 @@ export class Menu {
     return this.foods.map((f) => f.key)
   }
 
-  calculatRecipeNutritions(): void {
-    this.recipes.forEach((r) => {
+  calculatRecipeNutritions(recipes: Recipe[], foods: Food[]): void {
+    this.nutritions.clearValues();
+
+    // recipes
+    this.recipes.forEach((menuRecipe) => {
       
+      const recipe = recipes.find((recipe) => recipe.key == menuRecipe.key)
+
+      if (recipe) {
+        this.nutritions.kj += recipe.nutritions.kj / recipe.portions * menuRecipe.quantity
+        this.nutritions.kcal += recipe.nutritions.kcal / recipe.portions * menuRecipe.quantity
+        this.nutritions.protein += recipe.nutritions.protein / recipe.portions * menuRecipe.quantity
+        this.nutritions.carbs += recipe.nutritions.carbs / recipe.portions * menuRecipe.quantity
+        this.nutritions.fat += recipe.nutritions.fat / recipe.portions * menuRecipe.quantity
+      }
+
     })
+
+    // foods
+    this.foods.forEach((menuFood) =>{
+
+      const food = foods.find((food) => food.key == menuFood.key)
+
+      if(food) {
+        this.nutritions.kj += food.kj / food.base * menuFood.quantity
+        this.nutritions.kcal += food.kcal / food.base * menuFood.quantity
+        this.nutritions.protein += food.protein / food.base * menuFood.quantity
+        this.nutritions.carbs += food.carbs / food.base * menuFood.quantity
+        this.nutritions.fat += food.fat / food.base * menuFood.quantity
+      }
+    })
+
+    this.nutritions.kj = Math.round(this.nutritions.kj * 100) /100
+    this.nutritions.kcal = Math.round(this.nutritions.kcal * 100) /100
+    this.nutritions.protein = Math.round(this.nutritions.protein * 100) /100
+    this.nutritions.carbs = Math.round(this.nutritions.carbs * 100) /100
+    this.nutritions.fat = Math.round(this.nutritions.fat * 100) /100
   }
 }
--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { View, ScrollView, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, ScrollView, StyleSheet, Text } from "react-native";
 import Calendar from "../Components/Calendar/Calendar";
 import { Food } from "../Models/Food";
 import { Menu } from "../Models/Menu";
@@ -11,6 +11,7 @@ import { selectMenuItemByDate } from "../../store/redux/menu";
 import { useSelector } from "react-redux";
 import { Button } from "react-native-elements";
 import { SelectedItems } from "../Models/Menu";
+import { RecipeNutritions } from "../Models/RecipeNutritions";
 
 // Define the selector function
 const itemSelector = (item) => ({
@@ -19,56 +20,67 @@ const itemSelector = (item) => ({
   unit: item.unit,
 });
 
-
 const HomeScreen: React.FC = () => {
   const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
   const recipes: Recipe[] = useAppSelector(
     (state: RootState) => state.recipe.items
   );
   const foods: Food[] = useAppSelector((state: RootState) => state.food.items);
-  const [menu, setMenu] = useState<Menu>(useSelector(selectMenuItemByDate(date)) || new Menu(date))
+  const selectedMenu = useSelector(
+    selectMenuItemByDate(date)
+  );
 
-  console.log(menu);
+  const [menu, setMenu] = useState<Menu>(selectedMenu || new Menu(date));
+  const [nutritions, setNutritions] = useState<RecipeNutritions>(menu.nutritions);
+
+  // Update menu and nutritions whenever date changes
+  useEffect(() => {
+    setMenu(selectedMenu || new Menu(date));
+    setNutritions(selectedMenu?.nutritions || new RecipeNutritions());
+  }, [date, selectedMenu]);
+
+  const updateMenuAndNutritions = (updatedMenu: Menu) => {
+    updatedMenu.calculatRecipeNutritions(recipes, foods);
+    setMenu(updatedMenu);
+    setNutritions(updatedMenu.nutritions);
+  };
 
   const handleRecipesChange = (key: string) => {
-    console.log(key)
-    let mFood =  menu.recipes
-    let menuFoods = menu.recipes.map((f) => f.key)
+    let mFood = menu.recipes;
+    let menuFoods = menu.recipes.map((f) => f.key);
     if (menuFoods.includes(Number(key))) {
-      mFood = menu.recipes.filter((f) => f.key != Number(key))
+      mFood = menu.recipes.filter((f) => f.key != Number(key));
     } else {
-      let obj = new SelectedItems(Number(key), 0)
-      mFood = [...mFood, obj]
+      let obj = new SelectedItems(Number(key), 0);
+      mFood = [...mFood, obj];
     }
-    console.log(mFood)
-    setMenu((prevMenu) => new Menu(prevMenu.date, prevMenu.foods, mFood));
-    console.log(menu)
+    const updatedMenu = new Menu(menu.date, menu.foods, mFood);
+    updateMenuAndNutritions(updatedMenu);
   };
 
   const handleFoodsChange = (key: string) => {
-    console.log(key)
-    let mFood =  menu.foods
-    let menuFoods = menu.foods.map((f) => f.key)
+    let mFood = menu.foods;
+    let menuFoods = menu.foods.map((f) => f.key);
     if (menuFoods.includes(Number(key))) {
-      mFood = menu.foods.filter((f) => f.key != Number(key))
+      mFood = menu.foods.filter((f) => f.key != Number(key));
     } else {
-      let obj = new SelectedItems(Number(key), 0)
-      mFood = [...mFood, obj]
+      let obj = new SelectedItems(Number(key), 0);
+      mFood = [...mFood, obj];
     }
-    console.log(mFood)
-    setMenu((prevMenu) => new Menu(prevMenu.date, mFood, prevMenu.recipes));
-    console.log(menu)
+    const updatedMenu = new Menu(menu.date, mFood, menu.recipes);
+    updateMenuAndNutritions(updatedMenu);
   };
 
   const handleFoodInput = (item: SelectedItems) => {
     const updatedFood = menu.foods.map((book) => {
       if (book.key === item.key) {
-        return { ...book, quantity: item.quantity }; // Replace edition with a new value
+        return { ...book, quantity: item.quantity };
       }
       return book;
     });
-    setMenu((prevMenu) => new Menu(prevMenu.date, updatedFood, prevMenu.recipes));
-  }
+    const updatedMenu = new Menu(menu.date, updatedFood, menu.recipes);
+    updateMenuAndNutritions(updatedMenu);
+  };
 
   return (
     <View style={styles.screen}>
@@ -76,30 +88,35 @@ const HomeScreen: React.FC = () => {
         <Calendar date={date} setDate={setDate} />
         <View style={styles.selector}>
           {foods.length > 0 && (
-            <Selector 
+            <Selector
               items={foods.map((f) => itemSelector(f))}
               checkedItems={menu.foods}
               setCheckedItems={handleFoodsChange}
               setInput={handleFoodInput}
-              title="Potraviny" />
+              title="Potraviny"
+            />
           )}
-          
         </View>
         <View style={styles.selector}>
           {/* {recipes.length > 0 && (
-            <Selector 
-            items={recipes.map((f) => itemSelector(f))}
-            checkedItems={menu.recipes}
-            setCheckedItems={handleRecipesChange}
-            title="Recepty" />
+            <Selector
+              items={recipes.map((f) => itemSelector(f))}
+              checkedItems={menu.recipes}
+              setCheckedItems={handleRecipesChange}
+              title="Recepty"
+            />
           )} */}
         </View>
       </View>
       <View>
-        
+        <Text>KJ: {nutritions.kj}</Text>
+        <Text>KCal: {nutritions.kcal}</Text>
+        <Text>Bielkoviny: {nutritions.protein}</Text>
+        <Text>Sacharidy: {nutritions.carbs}</Text>
+        <Text>Tuky: {nutritions.fat}</Text>
       </View>
       <ScrollView>
-        <Button title={"Ulozit"}/>
+        <Button title={"Ulozit"} />
       </ScrollView>
     </View>
   );
