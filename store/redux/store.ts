c0e33a675d239c70d7c112748db4afcd595aa0cc
SakalikPeter <sakalikp@gmail.com>
2024-07-31 20:36:19 +0200

    fix store error

--- store/redux/store.ts
+++ store/redux/store.ts
@@ -1,67 +1,77 @@
-// store.js
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import AsyncStorage from "@react-native-async-storage/async-storage"; // Use AsyncStorage for React Native
-import foodReducer from "./food";
-import recipeReducer from "./recipe";
-import unitReducer from "./unit";
-import categoryReducer from "./category";
-import tagReducer from "./tag";
-import menuReducer from "./menu";
+import { configureStore } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import foodReducer from './food';
+import recipeReducer from './recipe';
+import unitReducer from './unit';
+import categoryReducer from './category';
+import tagReducer from './tag';
+import menuReducer from './menu';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 
-// Configuration for redux-persist
 const foodPersistConfig = {
-  key: "food",
+  key: 'food',
   storage: AsyncStorage,
 };
-const menuPersistConfig = {
-  key: "food",
-  storage: AsyncStorage,
-};
-const recipePersistConfig = {
-  key: "recipe",
-  storage: AsyncStorage,
-};
-const categoryPersistConfig = {
-  key: "category",
-  storage: AsyncStorage,
-}
-const tagPersistConfig = {
-  key: "tag",
-  storage: AsyncStorage,
-}
-const unitPersistConfig = {
-  key: "unit",
-  storage: AsyncStorage,
-}
 
-// Wrap the foodReducer with persistReducer
+const menuPersistConfig = {
+  key: 'menu',
+  storage: AsyncStorage,
+};
+
+const recipePersistConfig = {
+  key: 'recipe',
+  storage: AsyncStorage,
+};
+
+const categoryPersistConfig = {
+  key: 'category',
+  storage: AsyncStorage,
+};
+
+const tagPersistConfig = {
+  key: 'tag',
+  storage: AsyncStorage,
+};
+
+const unitPersistConfig = {
+  key: 'unit',
+  storage: AsyncStorage,
+};
+
 const persistedFoodReducer = persistReducer(foodPersistConfig, foodReducer);
 const persistedMenuReducer = persistReducer(menuPersistConfig, menuReducer);
-const persistedRecipeReducer = persistReducer(
-  recipePersistConfig,
-  recipeReducer
-)
-const persistedCategoryReducer = persistReducer(
-  categoryPersistConfig,
-  categoryReducer
-)
-const persistedTagReducer = persistReducer(tagPersistConfig, tagReducer)
-const persistedUnitReducer = persistReducer(unitPersistConfig, unitReducer)
+const persistedRecipeReducer = persistReducer(recipePersistConfig, recipeReducer);
+const persistedCategoryReducer = persistReducer(categoryPersistConfig, categoryReducer);
+const persistedTagReducer = persistReducer(tagPersistConfig, tagReducer);
+const persistedUnitReducer = persistReducer(unitPersistConfig, unitReducer);
 
-// Configure the Redux store
 const store = configureStore({
   reducer: {
-    food: persistedFoodReducer, // Use the persisted reducer
+    food: persistedFoodReducer,
     recipe: persistedRecipeReducer,
     unit: persistedUnitReducer,
     category: persistedCategoryReducer,
     tag: persistedTagReducer,
     menu: persistedMenuReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-// Create the persistor
 const persistor = persistStore(store);
-
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export { store, persistor };
